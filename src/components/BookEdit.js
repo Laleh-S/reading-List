8377e2c16dc19ab55a08f0efe30b498fb80b2c88
Laleh Shahidi <laleh.shahidi78@gmail.com>
2023-09-09 14:26:51 +0100

    BookEdit: (created form + handleSubmit event handler

--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -1,8 +1,26 @@
+import { useState } from "react";
+
 function BookEdit() {
+    const [title, setTitle] = useState('');
+
+    const handleChange = (event) => {
+        setTitle(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("new title is", title)
+    };
+
     return (
-        <div>
-            BookEdit
-        </div>
+        // 🀰🀰🀰 Form to edit books 🀰🀰🀰
+        <form className="book-edit" onSubmit={handleSubmit} >
+            <label>Title</label>
+            <input className="input" value={title} onChange={handleChange} />
+            <button className="button is-primary" >
+                Save
+            </button>
+        </form>
     )
 }
 
