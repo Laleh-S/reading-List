90cbb8439fc1bc6b334298b5adfe3644ae7fd5a2
Laleh Shahidi <laleh.shahidi78@gmail.com>
2023-09-09 12:41:10 +0100

    BookShow (created edit button  and the function to handle it)

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,14 +1,31 @@
+import { useState } from "react";
+import BookEdit from "./BookEdit";
+
 function BookShow({ singleBook, onDelete }) {
-    const handleClick = () => {
+    const [showEdit, setShowEdit] = useState(false);
+
+    const handleDeleteClick = () => {
         onDelete(singleBook.id);
     };
 
+    const handleEditClick = () => {
+        setShowEdit(!showEdit);
+    };
+
+    let content = <h3>{singleBook.title}</h3>; // This shows book title by default. 
+    if(showEdit) {   // if showEdit is true, we don't display the title and instead we show the <BookEdit /> 
+        content = <BookEdit />;
+    }
+
     return (
         <div className="book-show">
-            {singleBook.title}
+            <div>{content}</div>
             <div className="actions">
-                <button className="delete" onClick={handleClick} >
+                <button className="delete" onClick={handleDeleteClick} >
                     Delete
+                </button>
+                <button className="edit" onClick={handleEditClick} >
+                    Edit
                 </button>
             </div>
         </div>
