49d561d77b701dfe0156e210ecd9d84300dd45f5
Laleh Shahidi <laleh.shahidi78@gmail.com>
2023-11-21 22:21:21 +0000

    initial deploy

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("./db.json");
-const middlewares = jsonServer.defaults({
-    static: "./build",
+const express = require('express');
+const path = require('path');
+const jsonServer = require('json-server');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Serve the React app
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Use the JSON server
+app.use('/api', jsonServer.router('db.json'));
+
+// Serve the React app for any other routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-const port = process.env.PORT || 3001;
-server.use(middlewares);
-server.use(
-    jsonServer.rewriter({
-    "/api/*": "/$1",
-    })
-);
-server.use(router);
-server.listen(port, () => {
-    console.log("Server is running");
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
